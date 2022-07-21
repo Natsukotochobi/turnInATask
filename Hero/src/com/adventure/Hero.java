31db3486034e12d4f2b7fd6a48fdd8daffa09913
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-07-21 11:42:23 +0900

    サンプルコードを作成

--- Hero/src/com/adventure/Hero.java
+++ Hero/src/com/adventure/Hero.java
@@ -2,35 +2,56 @@ package com.adventure;
 
 public class Hero {
     private String name;
-    protected int hp = 80;
-    private final int MAX_HP = 100;
+    private int tmp_hp;
+    private int max_hp;
 
-    public Hero(String name){
+    public Hero(String name, int tmp_hp, int max_hp) {
         this.name = name;
+        this.tmp_hp = tmp_hp;
+        this.max_hp = max_hp;
     }
 
-    public void swingTheSword(LittleGhost e){
+    public void swingTheSword(LittleGhost littleGhost) {
         System.out.println(this.name + "は、剣を振った！");
-        e.hp -= 10;
+        littleGhost.hp -= 10;
         System.out.println("リトルゴーストに、10のダメージを与えた！");
     }
 
-    public void rest(int sec){
+    public void rest(int sec) {
         System.out.println(this.name + "は、" + sec + "秒間休憩した！");
-        this.hp += sec * 2;
-        if(hp > 100){
-            this.hp = MAX_HP;
+        this.tmp_hp += sec * 2;
+
+        if (this.tmp_hp > this.max_hp) {
+            this.tmp_hp = this.max_hp;
+        }
+        if (this.tmp_hp == this.max_hp) {
             System.out.println("体力が全回復した！");
-        }else{
+        } else {
             System.out.println("体力が" + sec * 2 + "回復した！");
         }
-
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getTmpHp() {
+        return tmp_hp;
+    }
 
+    public void setTmpHp(int tmp_hp) {
+        this.tmp_hp = tmp_hp;
+    }
+
+    public int getMaxHp() {
+        return max_hp;
+    }
+
+    public void setMaxHp(int max_hp) {
+        this.max_hp = max_hp;
+    }
 }
--- Hero/src/com/adventure/Main.java
+++ Hero/src/com/adventure/Main.java
@@ -5,15 +5,44 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Hero hero1 = new Hero("タロウ");
+        Hero heroTaro = new Hero("タロウ", 100, 100);
+        Hero heroJiro = new Hero("ジロウ", 150, 150);
+        Hero heroIchiro = new Hero("イチロー", 200, 200);
         LittleGhost littleGhost1 = new LittleGhost();
 
-        System.out.println(hero1.getName());
+        // 生成したHeroオブジェクトをListに追加
+        List<Hero> heroList = new ArrayList<>();
+            heroList.add(heroTaro);
+            heroList.add(heroJiro);
+            heroList.add(heroIchiro);
 
-        hero1.swingTheSword(littleGhost1);
-        hero1.rest(5);
-        System.out.println(hero1.hp);
-        littleGhost1.attack(hero1);
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Heroのステータス");
+        System.out.println("-----------------------------------------------------");
+        // Heroオブジェクトの各フィールドを表示
+        heroList.forEach(hero -> {
+            System.out.println("ヒーロー名: " + hero.getName()+"\n"+"最大HP："+hero.getMaxHp()+"\n"+"現在HP："+hero.getTmpHp());
+        });
+        System.out.println("-----------------------------------------------------");
+        System.out.println("戦闘開始！");
+        System.out.println("-----------------------------------------------------");
+
+        // リトルゴーストに攻撃、リトルゴーストからの反撃の処理を行う
+        heroList.forEach(hero -> {
+           hero.swingTheSword(littleGhost1);
+           littleGhost1.attack(hero);
+           hero.rest(5);
+        });
+        System.out.println("-----------------------------------------------------");
+        System.out.println("戦闘終了");
+        System.out.println("-----------------------------------------------------");
+
+        // 戦闘後のHeroオブジェクトの各フィールドを表示
+        heroList.forEach(hero -> {
+            System.out.println("ヒーロー名: " + hero.getName()+"\n"+"最大HP："+hero.getMaxHp()+"\n"+"現在HP："+hero.getTmpHp());
+        });
+
+        System.out.println("-----------------------------------------------------");
 
         for (int i = 1; i < 11; i++) {
             System.out.println("勇者は" + i + "回剣を振った！");
