a5a193f49c4239b4201c943569d537b2326cd472
Natsuko <makumaku2014@gmail.com>
2022-07-23 20:28:07 +0900

    変更をコミットする１回目

--- Hero/src/com/adventure/Main.java
+++ Hero/src/com/adventure/Main.java
@@ -1,11 +1,14 @@
 package com.adventure;
 
+import org.w3c.dom.html.HTMLTableRowElement;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Hero hero1 = new Hero("タロウ");
+        Hero hero2 = new Hero("ハナコ");
         LittleGhost littleGhost1 = new LittleGhost();
 
         System.out.println(hero1.getName());
@@ -19,14 +22,14 @@ public class Main {
             System.out.println("勇者は" + i + "回剣を振った！");
         }
 
-        List<String> playerList = new ArrayList<String>();
-            playerList.add("山田太郎");
-            playerList.add("田中花子");
-            playerList.add("斉藤次郎");
-        System.out.println(playerList);
+        List<Hero> heroList = new ArrayList<Hero>();
+            heroList.add(hero1);
+            heroList.add(hero2);
+            //playerList.add("斉藤次郎");
+        System.out.println(heroList);
 
-        for (String player : playerList ){
-            System.out.println("プレイヤーは" + player + "です。");
+        for (Hero hero : heroList ){
+            System.out.println("誕生した勇者は" + hero + "です。");
         }
 
     }
